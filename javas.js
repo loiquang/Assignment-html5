baba211ad5e8df2a2115658673c7ec8e2aa94e6b
loiquang <loidqps27284@fpt.edu.vn>
2023-04-11 15:59:45 +0700

    upload

---
+++ javas.js
@@ -0,0 +1,13 @@
+// var click_sp1 = document.getElementById("sp2");
+// var sp11 = document.getElementById('sp11');
+// var sp22 = document.getElementById("sp22");
+// click_sp1.onclick = function(){
+//     sp11.style.display ='none';
+// sp22.style.display = 'block';
+
+// }
+function hoa(){
+    document.getElementById("sp11").style.display = "none";
+    document.getElementById("sp22").style.display = "block";
+    alert('alooooo');
+}
